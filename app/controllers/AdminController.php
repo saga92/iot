559625981fd4f5557bacbfc14766aa874290671e
saga92 <yahao9210normal@126.com>
2015-05-03 22:34:43 +0800

    add total and check timeout function

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -1,6 +1,7 @@
 <?php
 
 use Iot\Models\Resource as Resource;
+use Iot\Models\History as History;
 
 class AdminController extends ControllerBase{
 
@@ -29,8 +30,35 @@ class AdminController extends ControllerBase{
             }
         }
     }
-    public function checkAction(){
+    public function checkTimeout($user_id){
         //检查是否资源到期
+        $phql = 'SELECT * FROM Iot\Models\History WHERE user_id = :user_id: and'
+            .'util_time < :util_time:';
+        $historys = $this->modelsManager->executeQuery(
+            $phql, array(
+                'user_id' => $user_id,
+                'util_time' => $util_time
+            )
+        );
+        foreach($historys as $history){
+            $res = Resource::find(
+                array(
+                    'id='.$history->resource_id,
+                )
+            );
+            $res->is_occupy = 1;
+            $res->save();
+        }
+    }
 
+    public function totalMoney($user_id){
+        $phql = "SELECT * FROM Iot\Models\History h LEFT OUTER JOIN Iot\Models\Resource r"
+            ." ON h.resource_id = r.id";
+        $result = $this->modelsManager->executeQuery($phql);
+        $total_money = 0;
+        foreach($result as $re){
+            $total_money += ($re->price * $re->month_num);
+        }
+        return $total_money;
     }
 }
